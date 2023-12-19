ded54a22a94b91d33a9022a1b66343e252bba207
Saurabh Yadav <sauraj.contact@gmail.com>
2023-12-19 19:32:25 +0530

    Temp Commit

--- dashboard/static/assets/js/components/core/coreComponents/home.js
+++ dashboard/static/assets/js/components/core/coreComponents/home.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import { TabTitle } from "../scripts/general_function";
+
+// API CALLER
+import { APICaller } from "../scripts/server";
+
 
 
 export function Home() {
     TabTitle('Yogya Capital')
+
+
+    const [indexData, setIndexData] = useState({});
+
+    useEffect(() => {
+        APICaller.FetchDefaultIndexData().then((res) => {
+            setIndexData(res);
+        });
+    }, []);
+
+
     return (
         <>
         
