3b662c55f56a2efd129da02b7f1c56d38a938151
rakesh shah <21amtics464@gmail.com>
2023-12-22 23:51:29 +0530

    side bar - branch testing

--- dashboard/static/assets/js/components/core/coreComponents/resources.js
+++ dashboard/static/assets/js/components/core/coreComponents/resources.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { TabTitle } from "../scripts/general_function";
+import { Sidebar } from "./sideBar";
 
 export function Resources() {
     TabTitle('Yogya Capital | Resources')
     return (
         <>
-            <h1>Resources</h1> 
+            <Sidebar />
+            {/* <h1>Resources</h1>  */}
         </>
     );
 }
