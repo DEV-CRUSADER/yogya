291b995ad230c3fb42c1292daba384aa8ea69af4
Aman Malik <amanmalik8777@gmail.com>
2023-12-25 00:52:42 +0530

    update

--- dashboard/static/assets/js/components/core/coreComponents/home/whyus.js
+++ dashboard/static/assets/js/components/core/coreComponents/home/whyus.js
@@ -1,20 +1,30 @@
 import React from "react";
 
-import {CSS} from "../../../../../../css/home.css";
+import { CSS } from "../../../../../../css/home.css";
 import theGame from "../../../../../img/skinIntheGame.png";
+import selling from "../../../../../img/misSelling.png";
 
-
-export function WhyUs (){
-    return(
-        <>
-        <div className="hero-section-3">
-        <h1>WHY US?</h1>
-        <div>
-            <img src = {theGame} alt="skinIntheGame"  />
+export function WhyUs() {
+  return (
+    <>
+      <div className="hero-section-3 d-flex row ">
+        <h1 className="main-head text-center ">WHY US?</h1>
+        <div className="hero-section-3i col text-center">
+          <img src={selling} alt="misSelling" />
+          <h2 className="head-text  "> Skin in the game </h2>
+          <p className="general-text ">
+          We only suggest products which fulfills your goals.
+          </p>
         </div>
-
+        <div className="hero-section-3i col text-center"> 
+          <img src={theGame} alt="skinIntheGame" />
+          <h2 className="head-text "> Skin in the game </h2>
+          <p className="general-text ">
+            We suggest products where we ourselves would invest own money along
+            with yours.
+          </p>
         </div>
-        </>
-    );
-
+      </div>
+    </>
+  );
 }
