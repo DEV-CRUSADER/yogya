fa6349630c73a5de8162451797c31152400dea59
rakesh shah <21amtics464@gmail.com>
2023-12-24 12:20:48 +0530

    side bar partial commit 1

---
+++ dashboard/static/assets/js/components/core/coreComponents/sidebar/pages/aboutUs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const AboutUs = () => {
+    return (
+        <div className='home'>
+            <h1>About Us</h1>
+        </div>
+    );
+};
+
+export const OurAim = () => {
+    return (
+        <div className='home'>
+            <h1>Aim</h1>
+        </div>
+    );
+};
+
+export const OurVision = () => {
+    return (
+        <div className='home'>
+            <h1>Vision</h1>
+        </div>
+    );
+};
---
+++ dashboard/static/assets/js/components/core/coreComponents/sidebar/pages/services.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export const Services = () => {
+    return (
+        <div className='services'>
+            <h1>Services</h1>
+        </div>
+    );
+};
+
+export const ServicesOne = () => {
+    return (
+        <div className='services'>
+            <h1>Service 1</h1>
+        </div>
+    );
+};
+
+export const ServicesTwo = () => {
+    return (
+        <div className='services'>
+            <h1>Service 2</h1>
+        </div>
+    );
+};
+
+export const ServicesThree = () => {
+    return (
+        <div className='services'>
+            <h1>Service 3</h1>
+        </div>
+    );
+};
+
+export const ServicesFour = () => {
+    return (
+        <div className='services'>
+            <h1>Service 4</h1>
+        </div>
+    );
+};
+
+export const ServicesFive = () => {
+    return (
+        <div className='services'>
+            <h1>Service 5</h1>
+        </div>
+    );
+};
