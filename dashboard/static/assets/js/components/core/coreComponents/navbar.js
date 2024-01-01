82846e4c0fed873a37b0f1c84b2ec476e74822a4
Saurabh Yadav <sauraj.contact@gmail.com>
2024-01-02 00:20:05 +0530

    Updated the nav load bug

--- dashboard/static/assets/js/components/core/coreComponents/navbar.js
+++ dashboard/static/assets/js/components/core/coreComponents/navbar.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
-  BrowserRouter as BrowserRouter,
-  HashRouter,
-  Route,
-  Link,
+	BrowserRouter as BrowserRouter,
+	HashRouter,
+	Route,
+	Link,
 } from "react-router-dom";
 
 
@@ -11,60 +11,78 @@ import {
 import { Colors } from "../../../../../css/color.css";
 import { Css } from "../../../../../css/navbar.css";
 
-export function Navbar(props) {
-  const nav_links = [
-    {
-      to: "/",
-      label: "Home",
-    },
-    {
-      to: "/resources",
-      label: "Resources",
-    },
-    // {
-    //   to: "/mutual-funds",
-    //   label: "Mutual Funds",
-    // },
-    {
-      to: "/contact",
-      label: "Contact Us",
-    },
-  ];
 
-  return (
-    <>
-      <nav
-        className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
-        style={{
-          background: "#000",
-          boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-        }}
-      >
-        <div className="container-fluid p-2 px-4">
-          <Link className="navbar-brand "  to="/" style={{fontWeight:"bold" ,fontFamily: "Lora"} }>
-            {" "}
-            Yogya Capital{" "}
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              {nav_links.map((link) => (
-                <Link key={link.to} className="nav-link" to={link.to}>
-                  {link.label}
-                </Link>
-              ))}
-            </ul>
-            {/* <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+export function Navbar(props) {
+
+	const [activeTab, setActiveTab] = useState(null);
+
+	useEffect(() => {
+		const index = nav_links.findIndex((link) => link.to === location.pathname);
+		setActiveTab(index);
+	}, [location.pathname]);
+
+	const nav_links = [
+		{
+			to: "/",
+			label: "Home",
+		},
+		{
+			to: "/resources",
+			label: "Resources",
+		},
+		// {
+		//   to: "/mutual-funds",
+		//   label: "Mutual Funds",
+		// },
+		{
+			to: "/contact",
+			label: "Contact Us",
+		},
+	];
+
+	const handleLinkClick = (index) => {
+		setActiveTab(index);
+	};
+
+	return (
+		<>
+			<nav
+				className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
+				style={{
+					background: "#000",
+					boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+				}}
+			>
+				<div className="container-fluid p-2 px-4">
+					<Link className="navbar-brand " to="/" style={{ fontWeight: "bold", fontFamily: "Lora" }}>
+						{" "}
+						Yogya Capital{" "}
+					</Link>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse " id="navbarSupportedContent">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+							{nav_links.map((link, index) => (
+								<Link
+									key={link.to}
+									className={`nav-link ${activeTab === index ? "active_tab" : ""}`}
+									to={link.to}
+									onClick={() => handleLinkClick(index)}
+								>
+									{link.label}
+								</Link>
+							))}
+						</ul>
+						{/* <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
               <li className="nav-but px-1">
                 <Link className=" btn btn-self" to="#">
                   Sign up
@@ -76,10 +94,10 @@ export function Navbar(props) {
                 </Link>
               </li>
             </ul> */}
-          </div>
-        </div>
-      </nav>
-      
-    </>
-  );
+					</div>
+				</div>
+			</nav>
+
+		</>
+	);
 }
