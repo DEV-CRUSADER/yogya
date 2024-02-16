a7a9b48920e4876b3f82805997484ae1986273b7
Saurabh Yadav <sauraj.contact@gmail.com>
2024-02-17 01:24:24 +0530

    updated the ui and fixed loading errors

--- dashboard/static/assets/js/components/dashboard/App.js
+++ dashboard/static/assets/js/components/dashboard/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { TabTitle } from "../core/scripts/general_function";
 
 
-export function HomeView()  {
+export function DashboardHomeView()  {
     TabTitle('Yogya | Home')
     return (
         <>
-            <div>
-                This is dashboard
+            <div 
+                className='d-flex justify-content-center align-items-center
+                    fs-5 fs-md-4 fs-xl-2 fs-xxl-1 fw-bold text-secondary text-uppercase w-100'
+                style={{height: '100vh'}}
+            >
+                UNDER DEVELOPMENT
             </div>
         </>
     );
